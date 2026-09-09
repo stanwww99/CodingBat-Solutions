47f47703cf2801f5b6761f887c4bcfbae1cc2e60
Stanley Weng <stanley@stnconsult.com>
2026-09-08 22:46:31 -0400

    Add new methods: cigarParty, teenSum, answerCell

--- Problem_Solutions.java
+++ Problem_Solutions.java
@@ -1,3 +1,4 @@
+package codingbat;
 
 public class Problem_Solutions {
     public String[] fizzBuzz(int start, int end) {
@@ -150,6 +151,30 @@ public class Problem_Solutions {
         return result;
     }
 
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+        if (isWeekend) {
+            // On weekends, only the lower bound matters
+            return cigars >= 40;
+        } else {
+            // On weekdays, cigars must be between 40 and 60 inclusive
+            return cigars >= 40 && cigars <= 60;
+        }
+    }
+    public int teenSum(int a, int b) {
+        if ((a >= 13 && a <= 19) || (b >= 13 && b <= 19)) {
+            return 19;
+        }
+        return a + b;
+    }
+    public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+        if (isAsleep) {
+            return false;
+        }
+        if (isMorning && !isMom) {
+            return false;
+        }
+        return true;
+    }
 
 
 }
